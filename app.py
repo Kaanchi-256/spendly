41b6927136dcc3776f054c66323b20b289a936b5
Kaanchi-256 <kumar.vedula@gmail.com>
2026-09-06 17:01:29 +0530

    analytics comming soon

--- app.py
+++ app.py
@@ -333,6 +333,12 @@ def profile():
     )
 
 
+@app.route("/analytics")
+@login_required
+def analytics():
+    return render_template("analytics.html")
+
+
 @app.route("/terms")
 def terms():
     return render_template("terms.html")
